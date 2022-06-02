c480427324b39b2600277f738fedd2dc5be0450c
Oscar Vasquez <oscar.vasquez@interconsumo.com.gt>
2022-06-02 11:40:42 -0600

    Prueba Logica

---
+++ PruebaLogica/Nodo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaLogica
+{
+    internal class Nodo
+    {
+        public String letra;
+        public int valor;
+        public Nodo(String letra, int valor) { 
+            this.letra = letra;
+            this.valor = valor;
+        }
+
+    }
+}
---
+++ PruebaLogica/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using PruebaLogica;
+using System.Collections;
+
+ArrayList lista = new ArrayList();
+
+Console.WriteLine("\nIngresa tus letras:");
+String letras = Console.ReadLine();
+Operaciones(letras);
+
+void Operaciones(String value){
+    
+    int posicion = 26;
+    int total = 0;
+    foreach (char letra in value)
+    {
+        if (posicion == 26)
+        {
+            lista.Add(new Nodo(letra.ToString(), posicion));
+            posicion--;
+        }
+        else {
+            for (int i = 0; i < lista.Count; i++) {
+                Nodo n = (Nodo)lista[i];
+                if (letra.ToString().Equals(n.letra))
+                {
+                    posicion = n.valor;
+                    break;
+                }
+            }
+
+            lista.Add(new Nodo(letra.ToString(), posicion));
+            posicion--;
+        }        
+    }
+
+
+    for (int i = 0; i < lista.Count; i++)
+    {
+        Nodo n = (Nodo)lista[i];
+        total = total +  n.valor;
+
+    }
+
+    Console.WriteLine("La suma total es: " + total);
+    Console.ReadLine();    
+}
+
